478793a1e222dadacab3c624ec32d2320a4a2660
Ronan Henry <10464258+RonanHenry@users.noreply.github.com>
2024-09-04 15:06:19 +0200

    chore: add php projects

---
+++ monorepo-builder.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
+
+return static function (MBConfig $mBConfig): void {
+  // Locations of the projects
+  $mBConfig->packageDirectories([
+    __DIR__ . '/lib',
+    __DIR__ . '/packages',
+  ]);
+
+  // Extra parts to add after the merge
+  $mBConfig->dataToAppend([
+    ComposerJsonSection::REQUIRE_DEV => [
+      'symplify/monorepo-builder' => '^11.2',
+    ],
+  ]);
+};
