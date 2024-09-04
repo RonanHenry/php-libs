b036ba8fce174d817e472325e16a84886b915489
Ronan Henry <10464258+RonanHenry@users.noreply.github.com>
2024-09-04 18:19:17 +0200

    chore: add formatter/linter for PHP files

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -7,15 +7,20 @@ use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $mBConfig): void {
   // Locations of the projects
-  $mBConfig->packageDirectories([
-    __DIR__ . '/lib',
-    __DIR__ . '/packages',
-  ]);
+  $mBConfig->packageDirectories(
+    [
+      __DIR__ . '/lib',
+      __DIR__ . '/packages',
+    ]
+  );
 
   // Extra parts to add after the merge
-  $mBConfig->dataToAppend([
-    ComposerJsonSection::REQUIRE_DEV => [
-      'symplify/monorepo-builder' => '^11.2',
-    ],
-  ]);
+  $mBConfig->dataToAppend(
+    [
+      ComposerJsonSection::REQUIRE_DEV => [
+        'squizlabs/php_codesniffer' => '^3.10',
+        'symplify/monorepo-builder' => '^11.2',
+      ],
+    ]
+  );
 };
